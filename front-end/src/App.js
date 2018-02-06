f267b2ab38b0fb9876a84bcdc75e7bf03946663c
helencho <helencho@ac.c4q.nyc>
2018-02-06 13:37:53 -0500

    added basic structures

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route } from 'react-router-dom';
+// import logo from './logo.svg';
+// import './App.css';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <nav>
+          <Link to='/'>Login</Link>{' '}
+        </nav>
+        <div>
+          <Route exact path='/' component={Home} />
+        </div>
       </div>
     );
   }
