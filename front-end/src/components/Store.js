3f8016526e84a8abefaa5073ac0d79738cf256f0
simongaviria1 <simongaviria@ac.c4q.nyc>
2018-02-06 16:52:59 -0500

    formatted Store.js to display properly on webpage.... no biggie

--- front-end/src/components/Store.js
+++ front-end/src/components/Store.js
@@ -5,18 +5,21 @@ class Store extends Component {
     calculateRate = props => {}
 
     render() {
-        console.log("Store: ", this.props)
-        
+        console.log("Store: ", this.props.secondHandLaptop)
+
         return (
             <div>
                 <h1>Store</h1>
                 <div>
                     <button name='secondHandLaptop' onClick={this.props.handleStorePowerUpCount}>Buy Second Hand Laptop
-                    </button>
+                    </button>Count: {this.props.store.secondHandLaptop.count}
+                    <br></br>
                     <button name='newLaptop' onClick={this.props.handleStorePowerUpCount}>Buy New Laptop
-                    </button>
+                    </button>Count: {this.props.store.newLaptop.count}
+                    <br></br>
                     <button name='desktop' onClick={this.props.handleStorePowerUpCount}>Buy Desktop
-                    </button>
+                    </button>Count: {this.props.store.desktop.count}
+                    <br></br>
                 </div>
             </div>
         );
